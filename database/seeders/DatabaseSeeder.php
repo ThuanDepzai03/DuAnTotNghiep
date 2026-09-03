ea5764de1731aa36fc61166e96709eec9ddf6926
Thuandepzai03 <thuanvillager24@gmail.com>
2026-09-04 03:22:31 +0700

    Thêm data khách hàng

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             AttributeSeeder::class,
             BannerSeeder::class,
             ProductSeeder::class,
+            DemoCustomerOrderSeeder::class,
             ReviewSeeder::class,
             AdminSeeder::class,
             UserSeeder::class,
