4e331a0e64904c6f4fda77d72c60c161b705c0fa
Thuandepzai03 <thuanvillager24@gmail.com>
2026-09-23 04:36:13 +0700

    sđá

--- app/Models/VariantAttributeValue.php
+++ app/Models/VariantAttributeValue.php
@@ -24,28 +24,4 @@ class VariantAttributeValue extends Model
     {
         return $this->belongsTo(AttributeValue::class, 'attribute_value_id');
     }
-}<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
-class VariantAttributeValue extends Model
-{
-    protected $table = 'variant_attribute_values';
-
-    protected $fillable = [
-        'product_variant_id',
-        'attribute_value_id',
-    ];
-
-    public function productVariant()
-    {
-        return $this->belongsTo(ProductVariant::class);
-    }
-
-    public function attributeValue()
-    {
-        return $this->belongsTo(AttributeValue::class);
-    }
 }
